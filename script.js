b2c0369cdf7703ec8a2ecb5cb6d4e294feb3346f
noxx <smc.8645@gmail.com>
2020-03-11 16:00:03 -0500

    Build a function to display numbers on the screen

--- script.js
+++ script.js
@@ -3,36 +3,14 @@ let operand__2 = "";
 let screen = document.getElementById("screen");
 let flag = false;
 
+
 //===== NUMBERS =================================
-let zero = document.getElementById("zero");
-zero.addEventListener("click", saveNumbers(0));
-
-let one = document.getElementById("1");
-one.addEventListener("click", saveNumbers(1));
-
-let two = document.getElementById("2");
-two.addEventListener("click", saveNumbers(2));
-
-let three = document.getElementById("3");
-three.addEventListener("click", saveNumbers(3));
-
-let four = document.getElementById("4");
-four.addEventListener("click", saveNumbers(4));
-
-let five = document.getElementById("5");
-five.addEventListener("click", saveNumbers(5));
-
-let six = document.getElementById("6");
-six.addEventListener("click", saveNumbers(6));
-
-let seven = document.getElementById("7");
-seven.addEventListener("click", saveNumbers(7));
-
-let eight = document.getElementById("8");
-eight.addEventListener("click", saveNumbers(8));
-
-let nine = document.getElementById("9");
-nine.addEventListener("click", saveNumbers(9));
+let numbers = ["zero", "1", "2", "3", "4", "5", "6", "7", "8", "9", "dot"];
+numbers.forEach(element => {
+    document.getElementById(element).addEventListener("click", () => {
+        saveNumbers(element);
+    })
+})
 //==============================================
 
 //===== OPERANDS ===============================
@@ -47,37 +25,28 @@ multiply.addEventListener("click", show);
 
 let divide = document.getElementById("divide");
 divide.addEventListener("click",  show);
-
-let squareRoot = document.getElementById("squareRoot");
-squareRoot.addEventListener("click",  show);
 //==============================================
 
 //===== OTHERS =================================
 let clear = document.getElementById("clear");
 clear.addEventListener("click", show);
 
+let erase = document.getElementById("erase");
+erase.addEventListener("click",  show);
+
 let equal = document.getElementById("equal");
 equal.addEventListener("click", show);
-
-let dot = document.getElementById("dot");
-dot.addEventListener("click",  show);
 //==============================================
 
 //===== OPERATIONS =============================
 function saveNumbers(number) {
-    if(flag) {
-        operand_2 += number;
-    }else {
-        operand_1 += number;
-        zero.textContent = "ola";
-        screen.textContent = operand_1;
-    }
+        screen.textContent += number.toString();
 }
-function addNumbers() {
-
+function addNumbers(e) {
+    screen.textContent += "Works 2";
 }
 
 function show() {
-
+    screen.textContent += a;
 }
 //==============================================
